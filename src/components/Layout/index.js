5ea1d234e1f3f5b547222fe0d0fe13c7406bc2b3
Scalro <rotichcaleb200@gmail.com>
2023-09-17 04:22:25 -0700

    Initial commit

---
+++ src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import SideBar from '../SideBar';
+import './index.scss';
+
+
+const Layout = () => {
+    return (
+        <>
+        <SideBar />
+        </>
+    )
+}
+
+export default Layout;
