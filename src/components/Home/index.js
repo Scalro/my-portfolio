003df068f201fbd050c165fa101e8d3d5c206d76
CALEB ROTICH <rotichcale200@gmail.com>
2023-09-21 10:34:39 -0400

    Modified

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,29 @@
 import './index.scss'
+import profileImage from '../../assets/images/profile.png'
 const Home = () => {
   return (
     <div className="home">
-      <h1>Home Page</h1>
+      <div className="left">
+        <img src={profileImage} alt="Caleb Rotich" />
+      </div>
+      <div className="right">
+        <h1>
+          <i>Hello,</i> I am <b>CALEB ROTICH</b>
+        </h1>
+        <hr />
+        <p>Software Engineer</p>
+        <p>
+          With a fervent dedication to software development, I possess a
+          versatile skill set encompassing Java, JavaScript, Python, HTML, CSS,
+          Dart, and their respective frameworks. Proficient in data structures
+          and algorithms, software project management, and system analysis, I
+          possess the expertise to create web, mobile, and desktop applications.
+          My passion for technology drives my commitment to delivering
+          innovative and impactful solutions.
+        </p>
+      </div>
     </div>
   )
 }
+
 export default Home
