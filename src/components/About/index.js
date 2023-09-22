6156a4d4711e4144f240cdf1096e619ad34cc9ce
CALEB ROTICH <rotichcaleb200@gmail.com>
2023-09-22 03:55:28 -0400

    modified

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,31 @@
-import './index.scss'
-const About = () => {
+import './index.scss' // Import the SCSS file
+import profileImage from '../../assets/images/profile.png' // Adjust the path to your profile image
+
+function About() {
   return (
-    <div className="home">
+    <div className="about">
       <div className="left">
-        {/* <img src={profileImage} alt="Caleb Rotich" /> */}
+        <img className="profile-image" src={profileImage} alt="Caleb Rotich" />
       </div>
       <div className="right">
-        <h1>
-          <i>Hello,</i> I am <b>CALEB ROTICH</b>
-        </h1>
+        <h1>About Me</h1>
         <hr />
-        <p>SCALROTECH</p>
-        <p>Graduated at University of Eastern Africa Baraton</p>
+        <p>Hello, I'm Caleb Rotich, a Software Engineer.</p>
+        <p>
+          I graduated from the University of Eastern Africa Baraton in August
+          2023.
+        </p>
+        <p>
+          I am passionate about software development and eager to demonstrate my
+          skills and expertise.
+        </p>
+        <p>
+          I am currently looking for opportunities to work on innovative and
+          impactful projects.
+        </p>
       </div>
     </div>
   )
 }
+
 export default About
